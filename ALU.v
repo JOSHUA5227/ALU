608185f39d5ac5d7927d475b2900bfa44cb95b7b
JOSHUA5227 <joshua.quinthino@gmail.com>
2026-05-04 11:13:00 +0000

    Added counter and counter reset logic

--- ALU.v
+++ ALU.v
@@ -1,8 +1,8 @@
-module ALU(# parameter WIDTH = 7)(OPA,OPB,CIN,CLK,RST,CE,MODE,INP_VALID,CMD,RES,OFLOW,COUT,G,L,E,ERR);
+module ALU(# parameter WIDTH = 8)(OPA,OPB,CIN,CLK,RST,CE,MODE,INP_VALID,CMD,RES,OFLOW,COUT,G,L,E,ERR);
 
 input wire clk,rst;
 
-input wire [WIDTH:0] OPA,OPB;
+input wire [WIDTH-1:0] OPA,OPB;
 
 input wire CIN;
 input wire CE;
@@ -16,6 +16,8 @@ output reg [2*WIDTH -1:0]RES;
 output reg OFLOW,COUT,G,L,E,ERR;
 
 reg [1:0] count; // 3 bit counter
+reg [4:0] current_operation; // for resetting counter
+
 
 always@(posedge clk or posedge rst)
 begin
@@ -29,7 +31,9 @@ begin
 	end
 	else if(CE)
 	begin
+		case{MODE,CMD}
 		
+		endcase		
 	end
 	else
 	begin
@@ -39,5 +43,40 @@ begin
                 {G,L,E} <= {G,L,E};
                 ERR <= ERR;
 	end
+end
+
+// storing previous operation
+always@(posedge clk or posedge rst)
+begin
+	if(rst)
+	begin
+		current_operation <= 5'b0;
+	end
+	else
+		current_operation <= {CMD,MODE};
+end
+
+
+// counter logic
+always@(posedge clk or posedge rst)
+begin
+	if(rst)
+	begin
+		count <=1'b0;
+	end
+	else if(current_operation == {CMD,MODE})
+	begin
+		else if(count_EN)
+		begin
+			if(count >= 3)
+				count <=1'b0;
+			else
+				count <= count + 1;
+		end
+		else
+			count <= 1'b0;
+	end
+	else
+			count <= 1'b0;
 end
 endmodule
