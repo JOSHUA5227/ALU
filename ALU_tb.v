9302ce3f5b65173b9d7a451a54604547e5490507
JOSHUA5227 <joshua.quinthino@gmail.com>
2026-05-09 05:25:27 +0000

    added drivers for the inputs

--- ALU_tb.v
+++ ALU_tb.v
@@ -1,15 +1,59 @@
+`define WIDTH = 8;
 module ALU_tb;
 
-'define WIDTH = 8;
+initial
+	CLK =1'b0;
 
-	task driver_random(output reg[WIDTH -1:0] OPA,OPB);
-		OPA = $urandom();
-		OPB = $urandom();
+always
+	#5 CLK = ~CLK; // time period 10
+
+
+// DRIVERS A and B
+	task driver_random_AB;
+	begin
+		OPA = $urandom;
+		OPB = $urandom;
+	end
 	endtask
 	
-	task driver_specific(input [WIDTH-1:0] start,stop,output reg[WIDTH -1:0] OPA,OPB);
+	task driver_specific_AB(input [WIDTH-1:0] start,stop);
+	begin
 		OPA = $urandom_range(start,stop);
 		OPB = $urandom_range(start,stop);
+	end
+	endtask
+
+// DRIVERS RESET
+	task driver_RST_high;
+              	RST = 1'b1;
+        endtask
+
+        task driver_RST_low;
+		RST = 1'b0;
+        endtask
+	
+// DRIVER CE	
+	 task driver_CE_high;
+                CE = 1'b1;
+        endtask
+
+        task driver_CE_low;
+                CE = 1'b0;
+        endtask
+
+
+// DRIVER CIN	
+	 task driver_CIN_high;
+                CIN = 1'b1;
+        endtask
+
+        task driver_CIN_low;
+                CIN = 1'b0;
+        endtask
+
+// DRIVER INP_VALID
+	task drive_INP_VALID(input [1:0] i);
+		INP_VALID = i;
 	endtask
 
 
